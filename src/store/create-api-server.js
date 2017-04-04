79f1621be76a77fb32894e7e51349042e94760c0
joehe <leanbrown@live.cn>
2017-04-04 23:48:03 +0800

    port 8989

--- src/store/create-api-server.js
+++ src/store/create-api-server.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === 'production'
-    ? 'http://vnode'
+    ? 'http://vnode:8989'
     : 'http://vnode.c75d723e5dfd54de0b419e31bf33da8c3.cn-shenzhen.alicontainer.com'
 
 export default axios
